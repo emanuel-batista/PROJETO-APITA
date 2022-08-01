2a54c62b6d91e9cabf3d805484f127b54dfd5c1c
leexavier18 <leticia.damim@gmail.com>
2022-08-01 14:34:00 -0300

    Page inicial

--- public/index.php
+++ public/index.php
@@ -49,9 +49,14 @@
  
 </div> 
  
+
+
+<div class="imgpageinicial">
+  <img src="../static/images/neymar.jpg" class="img-fluid" alt="...">
+
+  </div> 
  
- 
-<div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+<!-- <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -92,7 +97,7 @@
     <span class="visually-hidden">Next</span>
   </button>
 </div>
- 
+  -->
  
  
  <!-- <div id="carousel" class="carousel" data-bs-ride="carousel"> 

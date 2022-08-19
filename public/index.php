bcd5cc7f87eb40f12f6f6dbc4e36f26f3b3ff80e
Leexaavier <leticiadamim673@gmail.com>
2022-08-19 07:56:29 -0300

    a

--- public/index.php
+++ public/index.php
@@ -21,12 +21,12 @@
     <?php $posts = getPublishedPostsRecent(); 
     var_dump($_SESSION);
       ?> 
-    <div class="flex-container"> 
+    <div class="container"> 
       <div class="sub-container">
         <img src= "../static/images/imagens-noticia/<?php echo $posts[0]["IMG_NOTICIA"]?>" class="img-fluid" alt="..." class='img-hero' style='min-width: 500px; max-width: 700px; max-height: 300px; object-fit: cover; border-radius: 40px;'>
         <div class="hero-section">
           <h1 class="h1-hero"><strong><?php echo $posts[0]['TITULO_NOTICIA']; ?></strong></h1>
-          <p class="p-hero"><?php echo $posts[0]['CHAMADA_NOTICIA']; ?></p>
+          <p class="p-hero"><?php echo $posts[0]['CONTEUDO_NOTICIA']; ?></p>
           <button class='btn btn-warning'><a href="#" class='ver-mais-hero'>Ver Mais</a></button>
         </div>
         </img> 
@@ -48,19 +48,6 @@
     </div>
   </div>
 
-  <div class="sub-container-3">
-        <img src= "../static/images/imagens-noticia/<?php echo $posts[0]["IMG_NOTICIA"]?>" class="img-fluid" alt="..." class='img-hero' style='min-width: 500px; max-width: 700px; max-height: 300px; object-fit: cover; border-radius: 40px;'>
-        <div class="hero-section">
-          <h1 class="h1-hero"><strong><?php echo $posts[0]['TITULO_NOTICIA']; ?></strong></h1>
-          <p class="p-hero"><?php echo $posts[0]['CONTEUDO_NOTICIA']; ?></p>
-          <button class='btn btn-warning'><a href="#" class='ver-mais-hero'>Ver Mais</a></button>
-        </div>
-        </img> 
-      </div>
-    </div> 
-    </div>
-  </div>
-  
   <div class="container-2">
     <div class="text-2">
       <h2 class='h2-2'><strong> BOMBA!!!! NEYMAR REVELA QUE IRÁ FUGIR PARA A TURQUIA</strong></h2>

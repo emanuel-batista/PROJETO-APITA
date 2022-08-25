22b66c99dd2277bf5072337b1e56e0fc84d5b758
Leexaavier <leticiadamim673@gmail.com>
2022-08-25 09:18:30 -0300

    alteração not

--- public/vernoticia.php
+++ public/vernoticia.php
@@ -4,6 +4,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">    
     <?php include '../includes/config.php'; 
     ?> 
+    <link rel="stylesheet" href="../static/style/style2.css">  
     <link rel="stylesheet" href="../static/style/style.css">  
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
